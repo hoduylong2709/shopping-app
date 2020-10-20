e8291ce6cb2dd8bceb209ce267aa5f48bb3d4c03
longho <hoduylong411@gmail.com>
2020-10-20 09:40:10 +0700

    Get Add Page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -2,8 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
+/*
+* GET pages index
+*/
 router.get('/', (req, res) => {
   res.send('Admin area');
+});
+
+/*
+* GET add page
+*/
+router.get('/add-page', (req, res) => {
+  var title = "";
+  var slug = "";
+  var content = "";
+
+  res.render('admin/add_page', {
+    title: title,
+    slug: slug,
+    content: content
+  });
 });
 
 module.exports = router;
