7dd0659210472a7046906fc81443a2a9b7bcd5d1
longho <hoduylong411@gmail.com>
2020-10-20 08:08:11 +0700

    Get more packages

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator');
 const config = require('./config/database');
 const pages = require('./routes/pages');
 const adminPages = require('./routes/admin_pages');
@@ -22,6 +25,46 @@ app.set('view engine', 'ejs');
 
 // Set public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser middleware
+//
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// Express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// Express Validator middleware
+app.use(expressValidator({
+  errorFormatter: function (param, msg, value) {
+    var namspace = param.split('.'),
+      root = namspace.shift(),
+      formParam = root;
+
+    while (namspace.length) {
+      formParam += '[' + namspace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+// Express Messages middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // Set routes
 app.use('/admin/pages', adminPages);
