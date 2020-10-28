a7778b3231ab10760d7f75d3d39c6fb1ab69644d
longho <hoduylong411@gmail.com>
2020-10-28 10:37:20 +0700

    Display Page Content

--- routes/pages.js
+++ routes/pages.js
@@ -2,9 +2,38 @@ const express = require('express');
 
 const router = express.Router();
 
+// Get Page Model
+var Page = require('../models/page');
+
+/*
+ *  GET / 
+ */
 router.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Home'
+  Page.findOne({ slug: 'home' }, (err, page) => {
+    if (err) console.log(err);
+    res.render('index', {
+      title: page.title,
+      content: page.content
+    });
+  });
+});
+
+/*
+ *  GET a page
+ */
+router.get('/:slug', (req, res) => {
+  var slug = req.params.slug;
+
+  Page.findOne({ slug: slug }, (err, page) => {
+    if (err) console.log(err);
+    if (!page) {
+      res.redirect('/');
+    } else {
+      res.render('index', {
+        title: page.title,
+        content: page.content
+      });
+    }
   });
 });
 
